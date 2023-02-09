9bdf370c67ab074e770d9bafe10cbe9108496801
Vikash2806 <vikashrajendran2806@gmail.com>
2023-02-10 02:53:18 +0530

    refactor: file structure

--- Ex-1/Create-Dept-table.sql
+++
@@ -1,1 +0,0 @@
-create table DEPT("DEPTNO" number(3), "DNAME" varchar2(10), "LOC" varchar2(10));
--- Ex-1/Create-Emp-table.sql
+++
@@ -1,1 +0,0 @@
-create table EMP ("EMPNO" number(6), "EPNAME" varchar2(20), "JOB" varchar2(20), "MGR" number(10),"HIREDATE" date, "SAL" number(7), "COMM" varchar2(20), "DEPTNO" number(10)) ;
